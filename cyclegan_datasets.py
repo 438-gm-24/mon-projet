e4e52b45f6af4a4494b11807068d7ac8fba5d5a7
438-gm-24 <77722866+438-gm-24@users.noreply.github.com>
2021-03-26 17:26:43 +0100

    Update cyclegan_datasets.py

--- cyclegan_datasets.py
+++ cyclegan_datasets.py
@@ -3,13 +3,13 @@ DATASET_TO_SIZES = {
     'WFG2FG_train': 1502
 }
 
-"""The image types of each dataset. Currently only supports .jpg or .png"""
+
 DATASET_TO_IMAGETYPE = {
     'WFG2FG_train': '.jpg',
     
 }
 
-"""The path to the output csv file."""
+
 PATH_TO_CSV = {
     'WFG2FG_train': './mon-projet/input/WFG2FG_train.csv',
     
